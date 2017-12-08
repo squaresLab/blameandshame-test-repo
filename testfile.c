a351329b35a3a264fe18fddd2d1e9948ea4707bd
Jeremy Lacomis <jlacomis@cmu.edu>
2017-12-08 13:10:29 -0500

    Another change to testfun

--- testfile.c
+++ testfile.c
@@ -5,7 +5,11 @@ int testfun(int a, int b)
   int x;
   x = a + b;
   x *= x;
-  return x;
+
+  int y;
+  y = x * 2;
+
+  return (x + y);
 }
 
 int testfun2(int a, int b)
@@ -22,7 +26,7 @@ int main()
   int y;
   x = testfun(3, 4);
   y = testfun2(3, 4);
-  printf("%d\n", x);
+  printf("%d, %d\n", x, y);
 
   return 0;
 }
